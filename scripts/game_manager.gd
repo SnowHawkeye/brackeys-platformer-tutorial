03b88a17f79afb7ea7cf7abf74bf57ff8b6710db
SnowHawkeye <48864485+SnowHawkeye@users.noreply.github.com>
2024-11-16 03:35:26 +0100

    Implement score and finalize game

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,7 +1,8 @@
 extends Node
 
 var score = 0
+@onready var score_label: Label = $"Score label"
 
 func add_point():
 	score += 1
-	print(score)
+	score_label.text = "You collected " + str(score) + " coins."
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+@onready var audio_stream_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died!")
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
+	audio_stream_player.play()
 	timer.start()
 
 
